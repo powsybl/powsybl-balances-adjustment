70aacfc1a7253254b4e0e5642f4223cde3ded8e7
Sébastien MURGEY <sebastien.murgey@rte-france.com>
2020-01-02 08:22:36 +0100

    Small refactor and Javadoc update
    Signed-off-by: Sébastien MURGEY <sebastien.murgey@rte-france.com>
    Signed-off-by: benrejebmoh <mohamed.ben-rejeb@rte-france.com>

--- src/main/java/com/powsybl/balances_adjustment/util/NetworkArea.java
+++ src/main/java/com/powsybl/balances_adjustment/util/NetworkArea.java
@@ -9,16 +9,25 @@ package com.powsybl.balances_adjustment.util;
 import com.powsybl.iidm.network.Network;
 
 /**
- * NetworkArea is defined as a list of participating voltage levels and a list of border devices.
+ * NetworkArea defines an area for balances adjustment as a net position provider, calculaed on a Network object
  *
  * @author Ameni Walha {@literal <ameni.walha at rte-france.com>}
  * @author Sebastien Murgey {@literal <sebastien.murgey at rte-france.com>}
  */
 public interface NetworkArea {
     /**
+     * Computes the net position of the area on a given network object.
+     * Net position sign convention is positive when flows are leaving the area (export) and negative
+     * when flows feed the area (import).
+     *
+     * @param network Network on which to compute the net position
      * @return Sum of the flows leaving the area
      */
     double getNetPosition(Network network);
 
+    /**
+     * Utility method in the algorithm to allow cache mechanism to be reset, when changing
+     * the network object on which getNetPosition() is applied for example.
+     */
     void resetCache();
 }
