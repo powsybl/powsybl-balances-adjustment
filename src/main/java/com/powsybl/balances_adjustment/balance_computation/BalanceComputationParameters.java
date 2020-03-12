3c4a8d206722967f9faf5888eda1cce3693b1360
benrejebmoh <48012306+benrejebmoh@users.noreply.github.com>
2020-03-12 13:18:10 +0100

    Balance computation parameters (#5)
    Signed-off-by: Sébastien MURGEY <sebastien.murgey@rte-france.com>
    Signed-off-by: benrejebmoh <mohamed.ben-rejeb@rte-france.com>

--- src/main/java/com/powsybl/balances_adjustment/balance_computation/BalanceComputationParameters.java
+++ src/main/java/com/powsybl/balances_adjustment/balance_computation/BalanceComputationParameters.java
@@ -6,59 +6,130 @@
  */
 package com.powsybl.balances_adjustment.balance_computation;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.powsybl.commons.config.PlatformConfig;
+import com.powsybl.commons.extensions.*;
 import com.powsybl.loadflow.LoadFlowParameters;
+
+import java.util.Objects;
 
 /**
  * parameters for balance computation.
+ *
  * @author Ameni Walha {@literal <ameni.walha at rte-france.com>}
+ * @author Mohamed Ben Rejeb {@literal <mohamed.benrejeb at rte-france.com>}
  */
-public class BalanceComputationParameters {
+public class BalanceComputationParameters extends AbstractExtendable<BalanceComputationParameters> {
 
-    private final LoadFlowParameters loadFlowParameters;
+    public static final double DEFAULT_THRESHOLD_NET_POSITION = 1;
+    public static final int DEFAULT_MAX_NUMBER_ITERATIONS = 5;
 
     /**
      * Threshold for comparing net positions (given in MW).
      * Under this threshold, the network area is balanced
      */
-    private final double thresholdNetPosition;
+    private double thresholdNetPosition;
 
     /**
      * Maximum iteration number for balances adjustment
      */
-    private final int maxNumberIterations;
-
-    private static final double DEFAULT_THRESHOLD_NETPOSITION = 1;
-
-    private static final int DEFAULT_MAX_NUMBER_ITERATIONS = 5;
-
+    private int maxNumberIterations;
 
     /**
      * Constructor with default parameters
      */
     public BalanceComputationParameters() {
-        this(DEFAULT_THRESHOLD_NETPOSITION, DEFAULT_MAX_NUMBER_ITERATIONS, LoadFlowParameters.load());
+        this(DEFAULT_THRESHOLD_NET_POSITION, DEFAULT_MAX_NUMBER_ITERATIONS);
     }
 
     /**
      * Constructor with given parameters
      * @param threshold Threshold for comparing net positions (given in MW)
-     * @param max Maximum iteration number for balances adjustment
+     * @param maxNumberIterations Maximum iteration number for balances adjustment
      */
-    public BalanceComputationParameters(double threshold, int max, LoadFlowParameters loadFlowParameters) {
-        this.thresholdNetPosition = threshold;
-        this.maxNumberIterations = max;
-        this.loadFlowParameters = loadFlowParameters;
+    public BalanceComputationParameters(double threshold, int maxNumberIterations) {
+        this.thresholdNetPosition = checkThresholdNetPosition(threshold);
+        this.maxNumberIterations = checkMaxNumberIterations(maxNumberIterations);
+    }
+
+    private static final double checkThresholdNetPosition(double threshold) {
+        if (threshold < 0) {
+            throw new IllegalArgumentException("Threshold must be positive");
+        }
+        return threshold;
+    }
+
+    private static final int checkMaxNumberIterations(int maxNumberIterations) {
+        if (maxNumberIterations < 0) {
+            throw new IllegalArgumentException("The maximum number of iterations must be positive");
+        }
+        return maxNumberIterations;
+    }
+
+    /**
+     * A configuration loader interface for the RaoComputationParameters extensions loaded from the platform configuration
+     *
+     * @param <E> The extension class
+     */
+    public static interface ConfigLoader<E extends Extension<BalanceComputationParameters>> extends ExtensionConfigLoader<BalanceComputationParameters, E> {
+    }
+
+    private static final Supplier<ExtensionProviders<ConfigLoader>> SUPPLIER =
+            Suppliers.memoize(() -> ExtensionProviders.createProvider(ConfigLoader.class, "balance-computation-parameters"));
+
+    private LoadFlowParameters loadFlowParameters = new LoadFlowParameters();
+
+    /**
+     * Load parameters from platform default config.
+     */
+    public static BalanceComputationParameters load() {
+        return load(PlatformConfig.defaultConfig());
+    }
+
+    /**
+     * Load parameters from a provided platform config.
+     */
+    public static BalanceComputationParameters load(PlatformConfig platformConfig) {
+        Objects.requireNonNull(platformConfig);
+
+        BalanceComputationParameters parameters = new BalanceComputationParameters();
+        parameters.readExtensions(platformConfig);
+
+        parameters.setLoadFlowParameters(LoadFlowParameters.load(platformConfig));
+        return parameters;
+    }
+
+    private void readExtensions(PlatformConfig platformConfig) {
+        for (ExtensionConfigLoader provider : SUPPLIER.get().getProviders()) {
+            addExtension(provider.getExtensionClass(), provider.load(platformConfig));
+        }
     }
 
     public LoadFlowParameters getLoadFlowParameters() {
         return loadFlowParameters;
     }
 
+    public BalanceComputationParameters setLoadFlowParameters(LoadFlowParameters loadFlowParameters) {
+        this.loadFlowParameters = Objects.requireNonNull(loadFlowParameters);
+        return this;
+    }
+
     public double getThresholdNetPosition() {
         return thresholdNetPosition;
     }
 
+    public BalanceComputationParameters setThresholdNetPosition(double thresholdNetPosition) {
+        this.thresholdNetPosition = checkThresholdNetPosition(thresholdNetPosition);
+        return this;
+    }
+
     public int getMaxNumberIterations() {
         return maxNumberIterations;
+    }
+
+    public BalanceComputationParameters setMaxNumberIterations(int maxNumberIterations) {
+        this.maxNumberIterations = checkMaxNumberIterations(maxNumberIterations);
+        return this;
     }
 }
---
+++ src/test/java/com/powsybl/balances_adjustment/balance_computation/json_parameters/JsonBalanceComputationParametersTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.balances_adjustment.balance_computation.json_parameters;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.google.auto.service.AutoService;
+import com.powsybl.balances_adjustment.balance_computation.BalanceComputationParameters;
+import com.powsybl.commons.AbstractConverterTest;
+import com.powsybl.commons.extensions.AbstractExtension;
+import com.powsybl.loadflow.LoadFlowParameters;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Mohamed Ben Rejeb {@literal <mohamed.benrejeb at rte-france.com>}
+ */
+public class JsonBalanceComputationParametersTest extends AbstractConverterTest {
+
+    @Test
+    public void testDefaultBalanceComputationConfig() {
+        BalanceComputationParameters parameters = new BalanceComputationParameters();
+        BalanceComputationParameters.load();
+        assertEquals(parameters.getMaxNumberIterations(), BalanceComputationParameters.DEFAULT_MAX_NUMBER_ITERATIONS);
+        assertEquals(parameters.getThresholdNetPosition(), BalanceComputationParameters.DEFAULT_THRESHOLD_NET_POSITION, .01);
+    }
+
+    @Test
+    public void readError() {
+        try {
+            JsonBalanceComputationParameters.read(getClass().getResourceAsStream("/balanceComputationParametersError.json"));
+            Assert.fail();
+        } catch (AssertionError ignored) {
+        }
+    }
+
+    @Test
+    public void readSuccessful() {
+        BalanceComputationParameters parameters = JsonBalanceComputationParameters.read(getClass().getResourceAsStream("/balanceComputationParameters.json"));
+        assertEquals(11, parameters.getMaxNumberIterations());
+        assertEquals(2, parameters.getThresholdNetPosition(), .01);
+        LoadFlowParameters actualLoadflowParams =  parameters.getLoadFlowParameters();
+        assertEquals("DC_VALUES", actualLoadflowParams.getVoltageInitMode().toString());
+        assertTrue(actualLoadflowParams.isTransformerVoltageControlOn());
+        assertTrue(actualLoadflowParams.isPhaseShifterRegulationOn());
+        assertFalse(actualLoadflowParams.isNoGeneratorReactiveLimits());
+        assertFalse(actualLoadflowParams.isSpecificCompatibility());
+    }
+
+    @Test
+    public void readExtension() throws IOException {
+        BalanceComputationParameters parameters = JsonBalanceComputationParameters.read(getClass().getResourceAsStream("/balanceComputationParametersWithExtension.json"));
+        assertEquals(1, parameters.getExtensions().size());
+        assertNotNull(parameters.getExtension(DummyExtension.class));
+        assertNotNull(parameters.getExtensionByName("dummy-extension"));
+    }
+
+    @Test
+    public void roundTrip() throws IOException {
+        BalanceComputationParameters parameters = JsonBalanceComputationParameters.read(getClass().getResourceAsStream("/balanceComputationParameters.json"));
+        roundTripTest(parameters, JsonBalanceComputationParameters::write, JsonBalanceComputationParameters::read, "/balanceComputationParameters.json");
+    }
+
+    @Test
+    public void writeExtension() throws IOException {
+        BalanceComputationParameters parameters = new BalanceComputationParameters();
+        parameters.addExtension(DummyExtension.class, new DummyExtension());
+        writeTest(parameters, JsonBalanceComputationParameters::write, AbstractConverterTest::compareTxt, "/balanceComputationParametersWithExtension.json");
+    }
+
+    @Test
+    public void updateLoadFlowParameters() {
+        BalanceComputationParameters parameters = new BalanceComputationParameters();
+        JsonBalanceComputationParameters.update(parameters, getClass().getResourceAsStream("/balanceComputationParameters.json"));
+
+        assertEquals("DC_VALUES", parameters.getLoadFlowParameters().getVoltageInitMode().toString());
+        assertTrue(parameters.getLoadFlowParameters().isTransformerVoltageControlOn());
+        assertTrue(parameters.getLoadFlowParameters().isPhaseShifterRegulationOn());
+        assertFalse(parameters.getLoadFlowParameters().isSpecificCompatibility());
+        assertFalse(parameters.getLoadFlowParameters().isNoGeneratorReactiveLimits());
+    }
+
+    static class DummyExtension extends AbstractExtension<BalanceComputationParameters> {
+
+        DummyExtension() {
+            super();
+        }
+
+        @Override
+        public String getName() {
+            return "dummy-extension";
+        }
+    }
+
+    @AutoService(JsonBalanceComputationParameters.ExtensionSerializer.class)
+    public static class DummySerializer implements JsonBalanceComputationParameters.ExtensionSerializer<DummyExtension> {
+
+        @Override
+        public void serialize(DummyExtension extension, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeEndObject();
+        }
+
+        @Override
+        public DummyExtension deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            return new DummyExtension();
+        }
+
+        @Override
+        public String getExtensionName() {
+            return "dummy-extension";
+        }
+
+        @Override
+        public String getCategoryName() {
+            return "balance-computation-parameters";
+        }
+
+        @Override
+        public Class<? super DummyExtension> getExtensionClass() {
+            return DummyExtension.class;
+        }
+    }
+}
