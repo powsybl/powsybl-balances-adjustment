5840d48bbc8209e15aba295a0454e4ab0922fb82
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2021-04-19 16:44:28 +0200

    Improve DataExchanges new methods
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- src/main/java/com/powsybl/balances_adjustment/data_exchange/DataExchanges.java
+++ src/main/java/com/powsybl/balances_adjustment/data_exchange/DataExchanges.java
@@ -13,10 +13,10 @@ import com.powsybl.timeseries.*;
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
-//import java.lang.reflect.Array;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -175,14 +175,14 @@ public class DataExchanges {
         return timeSeriesById.get(timeSeriesId);
     }
 
-    public List<DoubleTimeSeries> getTimeSeriesWithId(String eicCode) {
-        Objects.requireNonNull(eicCode);
+    public Stream<DoubleTimeSeries> getTimeSeriesWithDomainId(String inOutDomainId) {
+        Objects.requireNonNull(inOutDomainId);
 
         return getTimeSeries().stream().filter(t -> {
             Map<String, String> tags = t.getMetadata().getTags();
-            return eicCode.equalsIgnoreCase(tags.get(DataExchangesConstants.IN_DOMAIN + "." + DataExchangesConstants.MRID)) ||
-                    eicCode.equalsIgnoreCase(tags.get(DataExchangesConstants.OUT_DOMAIN + "." + DataExchangesConstants.MRID));
-        }).collect(Collectors.toList());
+            return inOutDomainId.equalsIgnoreCase(tags.get(DataExchangesConstants.IN_DOMAIN + "." + DataExchangesConstants.MRID)) ||
+                    inOutDomainId.equalsIgnoreCase(tags.get(DataExchangesConstants.OUT_DOMAIN + "." + DataExchangesConstants.MRID));
+        });
     }
 
     public Stream<DoubleTimeSeries> getTimeSeriesStream(String inDomainId, String outDomainId) {
@@ -196,23 +196,25 @@ public class DataExchanges {
         });
     }
 
-    public List<String> getCountriesId(String eicCode, Instant instant) {
-        ArrayList<String> eicCountries = new ArrayList<>();
-        List<DoubleTimeSeries> ts = getTimeSeriesWithId(eicCode);
+    public Map<String, Double> getNetPositionsWithInDomainId(String inDomainId, Instant instant) {
+        return getNetPositionsWithInDomainId(inDomainId, instant, true);
+    }
 
-        for (DoubleTimeSeries doubleTimeSeries : ts) {
-            Map<String, String> tags = doubleTimeSeries.getMetadata().getTags();
-            String inDomainId = tags.get(DataExchangesConstants.IN_DOMAIN + "." + DataExchangesConstants.MRID);
-            String outDomainId = tags.get(DataExchangesConstants.OUT_DOMAIN + "." + DataExchangesConstants.MRID);
-            if (getNetPosition(inDomainId, outDomainId, instant) != 0) {
-                if (inDomainId.equalsIgnoreCase(eicCode) && !eicCountries.contains(outDomainId)) {
-                    eicCountries.add(outDomainId);
-                } else if (outDomainId.equalsIgnoreCase(eicCode) && !eicCountries.contains(inDomainId)) {
-                    eicCountries.add(inDomainId);
-                }
-            }
-        }
-        return eicCountries;
+    public Map<String, Double> getNetPositionsWithInDomainId(String inDomainId, Instant instant, boolean exceptionOutOfBound) {
+        return getTimeSeriesWithDomainId(inDomainId)
+                .map(doubleTimeSeries -> {
+                    Map<String, String> tags = doubleTimeSeries.getMetadata().getTags();
+                    String tmpInDomainId = tags.get(DataExchangesConstants.IN_DOMAIN + "." + DataExchangesConstants.MRID);
+                    String tmpOutDomainId = tags.get(DataExchangesConstants.OUT_DOMAIN + "." + DataExchangesConstants.MRID);
+                    if (tmpInDomainId.equals(inDomainId)) {
+                        return tmpOutDomainId;
+                    } else if (tmpOutDomainId.equals(inDomainId)) {
+                        return tmpInDomainId;
+                    }
+                    throw new AssertionError();
+                })
+                .distinct()
+                .collect(Collectors.toMap(Function.identity(), outDomainId -> getNetPosition(inDomainId, outDomainId, instant, exceptionOutOfBound)));
     }
 
     public List<DoubleTimeSeries> getTimeSeries(String inDomainId, String outDomainId) {
